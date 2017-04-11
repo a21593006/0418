04225485b3a4bbccc9ec5752346036493a285003
Chang Kai Lo <a21593006@gmail.com>
2017-04-11 20:22:05 +0800

    Signed-off-by: Chang Kai Lo <a21593006@gmail.com>

---
+++ ex-十九乘十九乘法表/十九乘十九乘法表/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        For i As Integer = 1 To 19
+            For j As Integer = 1 To 19
+                TextBox1.Text = TextBox1.Text & i & "*" & j & "=" & i * j & " "
+            Next
+            TextBox1.Text = TextBox1.Text & vbNewLine
+        Next
+    End Sub
+End Class
